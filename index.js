34b261949ca0de7ae21b6fd7fda0b239491a3ff9
Sebastien <delsebastienjames@gmail.com>
2021-05-16 14:40:10 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+app.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    //console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(400).json({ message: "Page not found" });
+});
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
